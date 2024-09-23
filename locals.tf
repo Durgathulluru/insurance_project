67c64b1daccfdad946f4fd3d170bfbd0e1965ee9
Ops_Gudie <142125732+Durgathulluru@users.noreply.github.com>
2024-09-23 04:06:56 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ ami_id = "ami-080e1f13689e07408"
 vpc_id = "vpc-048db6e177c9b2ba7"
 ssh_user = "ubuntu"
 key_name = "key1" #[this is the name of the key in aws"]
-private_key = data.vault_generic_secret.ssh_key.data["private_key"]
+private_key = data.vault_generic_secret.ssh_key.data["ssh_key"]
 }
