50bfa359277e2d3bd056fc8813f45beab3355920
Ops_Gudie <142125732+Durgathulluru@users.noreply.github.com>
2024-09-23 04:07:49 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = data.vault_generic_secret.aws_key.data["access_key"]
-  secret_key = data.vault_generic_secret.aws_pass.data["secrete_access_key"]
+  access_key = data.vault_generic_secret.aws_key.data["aws_key"]
+  secret_key = data.vault_generic_secret.aws_pass.data["aws_pass"]
 }
 provider "vault" {
    address = "http://172.31.43.136:8200"
 }
-
