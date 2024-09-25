19d66250a301f5d3b9b628351ce447f0883cce87
Ops_Gudie <142125732+Durgathulluru@users.noreply.github.com>
2024-09-25 03:55:38 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,17 +1,15 @@
 data "vault_generic_secret" "aws_key" {
       path = "secret/aws_key"
 }
+
 data "vault_generic_secret" "aws_pass" {
       path = "secret/aws_pass"
 }
+
 data "vault_generic_secret" "ssh_key" {
       path = "secret/ssh_key"
 }
-data "aws_secretsmanager_secret" "db_credentials" {
-  name = "db_credentials"
-}
 
-data "aws_secretsmanager_secret_version" "db_credentials" {
-  secret_id = data.aws_secretsmanager_secret.db_credentials.id
+data "vault_generic_secret" "db_credentials" {
+      path = "secret/db_credentials"
 }
-
