cf98e2516ba0bb22b003ef647a65750804f20e9d
Ops_Gudie <142125732+Durgathulluru@users.noreply.github.com>
2024-09-30 02:31:00 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   ami_id = "ami-0e86e20dae9224db8"
-  vpc_id = "vpc-0f1b3f4e723fbaa77"
+  vpc_id = "vpc-0b8c2bd824f1b2e70"
   ssh_user = "ubuntu"
   key_name = "key1" # This is the name of the key in AWS
   private_key_path = "~/insurance/key1" # Path to the private key file
-  vault_addr = "http://172.31.80.169:8200"
+  vault_addr = "http://172.31.82.218:8200"
 }
