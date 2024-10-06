1af331a810898e6b5bab56db51760a1f468d889e
Ubuntu <ubuntu@ip-172-31-33-207.ec2.internal>
2024-10-06 22:16:29 +0000

    updated locals.tf file

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  ami_id = "ami-0e86e20dae9224db8"
-  vpc_id = "vpc-0b8c2bd824f1b2e70"
+  ami_id = "ami-0866a3c8686eaeeba"
+  vpc_id = "vpc-0c3e08be7810099cf"
   ssh_user = "ubuntu"
   key_name = "key1" # This is the name of the key in AWS
-  private_key_path = "~/insurance/key1" # Path to the private key file
-  vault_addr = "http://172.31.82.218:8200"
+  private_key_path = "~/.ssh/key1" # Path to the private key file
+  vault_addr = "http://172.31.33.207:8200"
 }
