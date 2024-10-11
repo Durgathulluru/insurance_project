68ca635c052d45d48c8e6facf50dbe387be5c88f
Ubuntu <ubuntu@ip-172-31-90-179.ec2.internal>
2024-10-11 08:48:15 +0000

    updated the vault addr in local.tf

--- locals.tf
+++ locals.tf
@@ -4,5 +4,5 @@ locals {
   ssh_user = "ubuntu"
   key_name = "key1" # This is the name of the key in AWS
   private_key_path = "/var/lib/jenkins/key1" # Path to the private key file
-#  vault_addr = "http://172.31.90.179:8200"
+  vault_addr = "http://172.31.90.179:8200"
 }
